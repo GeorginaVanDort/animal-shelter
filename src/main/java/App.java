a1b1bce6cee144afbf2e84007e897c62374ac7d8
Georgina Van Dort and Shelby Clayton <shelby@>
2016-09-21 11:56:37 -0700

    add template files and start to app.java and index.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
-import java.util.HashMap;
+
 import java.util.Map;
+import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -11,8 +12,81 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      //model.put("template", "templates/name.vtl");
+      model.put("template", "templates/index.vtl");
+      model.put("doctors", Doctor.all());
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("doctors/:id/patients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Doctor doctor = Doctor.find(Integer.parseInt(request.params(":id")));
+      model.put("doctor", doctor);
+      model.put("template", "templates/doctor-patients-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/patients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("patients", Patient.all());
+      model.put("template", "templates/patients.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/patients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Doctor doctor = Doctor.find(Integer.parseInt(request.queryParams("doctor")));
+
+      String name = request.queryParams("name");
+      String illness = request.queryParams("illness");
+      Patient newPatient = new Patient(name, illness, doctor.getId());
+      newPatient.save();
+
+      model.put("doctor", doctor);
+      model.put("patient", newPatient);
+      model.put("template", "templates/patient-success.vtl");
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/patients/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Patient patient = Patient.find(Integer.parseInt(request.params(":id")));
+      model.put("patient", patient);
+      model.put("template", "templates/patient.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/doctors/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/doctor-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/doctors", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String speciality = request.queryParams("speciality");
+      Doctor newDoctor = new Doctor(speciality, name);
+      newDoctor.save();
+      model.put("doctor", newDoctor);
+      model.put("template", "templates/doctor-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/doctors", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("doctors", Doctor.all());
+      model.put("template", "templates/doctors.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/doctors/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Doctor doctor = Doctor.find(Integer.parseInt(request.params(":id")));
+      model.put("doctor", doctor);
+      model.put("template", "templates/doctor.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
